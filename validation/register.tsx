84f2f68f03d22b05e764daee88382cf76a2228de
cberto <bertolanicami@gmail.com>
2026-05-16 13:50:29 -0300

    fix contraseña y redirect

--- validation/register.tsx
+++ validation/register.tsx
@@ -1,6 +1,7 @@
 import * as Yup from "yup";
 const regex_email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const regex_password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+const regex_password =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-={}[\]:;"'<>,.?/\\|`~]).{8,}$/;
 
 export const registerValidationSchema = Yup.object({
   name: Yup.string().min(2, "Mínimo 2 caracteres").required("Requerido"),
@@ -9,7 +10,10 @@ export const registerValidationSchema = Yup.object({
     .email("Email inválido")
     .required("Requerido"),
   password: Yup.string()
-    .matches(regex_password, "Contraseña inválida")
+    .matches(
+      regex_password,
+      "Debe tener mayúscula, minúscula, número, símbolo y mínimo 8 caracteres",
+    )
     .min(8, "Mínimo 8 caracteres")
     .required("Requerido"),
   confirmPassword: Yup.string()
