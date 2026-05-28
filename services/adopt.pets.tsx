034c6cc00e7e7f747b4344b1de333b232683aa33
cberto <bertolanicami@gmail.com>
2026-05-28 19:20:45 -0300

    Fix adopcion

--- services/adopt.pets.tsx
+++ services/adopt.pets.tsx
@@ -1,28 +1,13 @@
-import { ErrorGeneric } from "@/components/utils/catchErrors";
 import { AdoptForm } from "@/types/adoptar";
 import axiosInstance from "./axios";
+import { requestSafe } from "./request";
 import { User } from "@/types/user";
 
 const axios = axiosInstance;
 
-type ResponseAxios = {
-  ok: boolean;
-  data: User | null;
-  status: number;
-};
-export const submitAdoption: (
-  values: AdoptForm,
-) => Promise<ResponseAxios | undefined> = async (values: AdoptForm) => {
-  try {
-    const response = await axios.post("pet/adoptar", values,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    return { ok: true, data: response.data, status: response.status };
-  } catch (error) {
-    ErrorGeneric(error);
-  }
-};
+export const submitAdoption = (values: AdoptForm) =>
+  requestSafe<User>(() =>
+    axios.post("pet/adoptar", values, {
+      headers: { "Content-Type": "application/json" },
+    }),
+  );
