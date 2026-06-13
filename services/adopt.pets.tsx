949a4c09a8389c4cc77ca36a6b78b6c5b29da626
cberto <bertolanicami@gmail.com>
2026-06-13 19:19:51 -0300

    refactor: replace requestSafe with request in submitAdoption function

--- services/adopt.pets.tsx
+++ services/adopt.pets.tsx
@@ -1,12 +1,12 @@
 import { AdoptForm } from "@/types/adoptar";
 import axiosInstance from "./axios";
-import { requestSafe } from "./request";
+import { request } from "./request";
 import { User } from "@/types/user";
 
 const axios = axiosInstance;
 
 export const submitAdoption = (values: AdoptForm) =>
-  requestSafe<User>(() =>
+  request<User>(() =>
     axios.post("pet/adoptar", values, {
       headers: { "Content-Type": "application/json" },
     }),
